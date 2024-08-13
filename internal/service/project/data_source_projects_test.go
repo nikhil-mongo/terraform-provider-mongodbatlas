a171da17c23536b80877c55f9c5569a40a2563da
nikhil-mongo <50577166+nikhil-mongo@users.noreply.github.com>
2024-08-13 08:39:48 +0000

    build(deps): bump go.mongodb.org/atlas-sdk

--- internal/service/project/data_source_projects_test.go
+++ internal/service/project/data_source_projects_test.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"testing"
 
-	"go.mongodb.org/atlas-sdk/v20231115006/admin"
+	"go.mongodb.org/atlas-sdk/v20240805001/admin"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
